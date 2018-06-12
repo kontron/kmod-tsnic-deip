cd383dd3745f7eb64ac37630e7234e901a38cdb5
Michael Walle <michael.walle@kontron.com>
2018-06-12 15:36:51 +0200

    vendor release 1.3.1
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- deipce/deipce_main.h
+++ deipce/deipce_main.h
@@ -27,7 +27,7 @@
 #include "deipce_types.h"
 
 #define DRV_NAME        "deipce"
-#define DRV_VERSION     "1.0.0"
+#define DRV_VERSION     "1.3.1"
 
 struct deipce_drv_priv *deipce_get_drv_priv(void);
 
